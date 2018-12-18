b0ef9d1cad67611e76f8a4e869f1ee2c0d435d32
Ivan <i.poblete915@gmail.com>
2018-12-17 17:17:54 -0800

    added Modal

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -3,16 +3,19 @@
     <div>
       <h2>Search for a Joke</h2>
       <JokesList/>
+      <Modal/>
     </div>
   </section>
 </template>
 
 <script>
 import JokesList from './JokesList.vue';
+import Modal from '../shared/Modal.vue';
 
 export default {
   components: {
-    JokesList
+    JokesList,
+    Modal
   }
 };
 </script>
---
+++ src/components/shared/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="modal" @click="onClose">
+      <div class="content" @click.stop="">
+        <button class="close" @click="onClose">X</button>
+        <form @submit.prevent="onSubmit(joke)">
+          <label>
+            Joke:
+            <textarea name="message" rows="10" cols="30"/>
+          </label>
+          <button> {{label || 'Add'}} </button>
+        </form>   
+      </div>   
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
