41910ac6e448bd0a1549f877b889ce7710f0726b
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-12-19 09:17:55 -0800

    added delete button to jokes in MyJokes and working on functionality

--- src/services/api.js
+++ src/services/api.js
@@ -70,5 +70,9 @@ export default {
   getMyJokes() {
     return fetch('/api/jokes', getOptions('GET'))
       .then(response => response.json());
+  },
+  deleteMyJoke(id) {
+    return fetch('/api/jokes/id', getOptions('DELETE'))
+      .then(response => response.json());
   }
 };
