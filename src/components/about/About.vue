d2fcdb5d25e01a67980611195780f7110352bddf
JeiWolfei <jvampy22@gmail.com>
2018-12-19 10:58:56 -0800

    working on about us

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,10 +1,18 @@
 <template>
   <section>
-    <h3>About Us</h3>
-    <p>Hans</p>
-    <p>Teonna</p>
-    <p>Jei</p>
-    <p>Ivan</p>
+    <div>
+      <h3>About Us</h3>
+      We are team Octopus, a small group of up and coming developers at Alchemy
+      Code Lab. We built this "Jokester" app to bring laughter into our stressful lives.
+      We hope you enjoy the dad jokes, they are our favorites. 
+    </div>
+    <div>
+      <p>Hans</p>
+      <p>Teonna</p>
+      <p>Jei</p>
+      <p>Ivan</p>
+    </div>
+
   </section>
 </template>
 
